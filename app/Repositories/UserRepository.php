9e1656345b99b477c1ceb205f1d67816553fa0c6
dimonchikua96 <dimonchikua96@gmail.com>
2018-12-22 22:42:44 +0200

    init

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: samsung
+ * Date: 20.11.2018
+ * Time: 21:37
+ */
+
+namespace App\Repositories;
+
+
+class UserRepository
+{
+    public function create(){
+        return false;
+    }
+
+}
